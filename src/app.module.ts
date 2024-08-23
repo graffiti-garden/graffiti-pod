08f613f7edfd842c1ef45b2beebb6da11d3641ad
theia <theia@mit.edu>
2024-08-22 23:38:13 -0400

    no dht

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from "@nestjs/common";
 import { StoreModule } from "./store/store.module";
 import { MongooseModule } from "@nestjs/mongoose";
-// import { DhtModule } from "./dht/dht.module";
 import { ScheduleModule } from "@nestjs/schedule";
 
 export const RootMongooseModule = MongooseModule.forRoot("mongodb://mongo");
 
 @Module({
-  imports: [
-    StoreModule,
-    RootMongooseModule,
-    // DhtModule,
-    ScheduleModule.forRoot(),
-  ],
+  imports: [StoreModule, RootMongooseModule, ScheduleModule.forRoot()],
 })
 export class AppModule {}
--- src/dht/dht.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { DhtService } from "./dht.service";
-import { DhtController } from "./dht.controller";
-
-@Module({
-  providers: [DhtService],
-  controllers: [DhtController],
-})
-export class DhtModule {}

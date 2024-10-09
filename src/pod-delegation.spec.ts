518aeda05c8e6d9c7ebb853b36eaeff74ecf3693
theia <theia@mit.edu>
2024-10-09 15:54:21 -0400

    pod delegation work

---
+++ src/pod-delegation.spec.ts
@@ -0,0 +1,163 @@
+import { it, expect } from "vitest";
+import PodDelegation from "./pod-delegation";
+import type { GraffitiLocalObject } from "./types";
+import type { USER_SETTINGS_SCHEMA } from "./schemas";
+import Ajv from "ajv";
+import { randomString } from "./test-utils";
+
+it("one pod delegates anything", async () => {
+  const pod = `https://${randomString()}.com`;
+  const settings = {
+    value: {
+      podDelegation: [
+        {
+          pod,
+          schema: {
+            type: "object",
+          },
+        },
+      ],
+    },
+    channels: [],
+  } satisfies GraffitiLocalObject<typeof USER_SETTINGS_SCHEMA>;
+
+  const manager = new PodDelegation(new Ajv());
+  expect(manager.isPodDelegated(settings, pod, {})).toBe(true);
+  expect(
+    manager.isPodDelegated(settings, pod, {
+      something: "else",
+      asdf: 1234,
+      cool: ["beans", 69],
+    }),
+  ).toBe(true);
+  expect(
+    manager.isPodDelegated(settings, `https://${randomString()}.com`, {}),
+  ).toBe(false);
+});
+
+it("one pod delegates some things, another pod delegates others", async () => {
+  const pod1 = `https://${randomString()}.com`;
+  const pod2 = `https://${randomString()}.com`;
+  const settings = {
+    value: {
+      podDelegation: [
+        {
+          pod: pod1,
+          schema: {
+            type: "object",
+            properties: {
+              something: { type: "string" },
+            },
+            required: ["something"],
+          },
+        },
+        {
+          pod: pod2,
+          schema: {
+            type: "object",
+            properties: {
+              otherthings: { type: "string" },
+            },
+            required: ["otherthings"],
+          },
+        },
+      ],
+    },
+    channels: [],
+  } satisfies GraffitiLocalObject<typeof USER_SETTINGS_SCHEMA>;
+
+  const manager = new PodDelegation(new Ajv());
+  expect(
+    manager.isPodDelegated(settings, pod1, {
+      something: "else",
+      whatever: "stuff",
+    }),
+  ).toBe(true);
+  expect(
+    manager.isPodDelegated(settings, pod1, {
+      something: 1234,
+    }),
+  ).toBe(false);
+  expect(
+    manager.isPodDelegated(settings, pod1, {
+      otherthings: "else",
+    }),
+  ).toBe(false);
+  expect(
+    manager.isPodDelegated(settings, pod2, {
+      otherthings: "else",
+    }),
+  ).toBe(true);
+  expect(
+    manager.isPodDelegated(settings, pod2, {
+      otherthings: {},
+    }),
+  ).toBe(false);
+  expect(
+    manager.isPodDelegated(settings, pod2, {
+      something: "else",
+    }),
+  ).toBe(false);
+});
+
+it("which pod delegates what", async () => {
+  const pod1 = `https://${randomString()}.com`;
+  const pod2 = `https://${randomString()}.com`;
+
+  const settings = {
+    value: {
+      podDelegation: [
+        {
+          pod: pod1,
+          schema: {
+            type: "object",
+            required: ["value"],
+            properties: {
+              value: {
+                type: "object",
+                properties: {
+                  something: { type: "string" },
+                },
+                required: ["something"],
+              },
+            },
+          },
+        },
+        {
+          pod: pod2,
+          schema: {
+            type: "object",
+            required: ["value"],
+            properties: {
+              value: {
+                type: "object",
+              },
+            },
+          },
+        },
+      ],
+    },
+    channels: [],
+  } satisfies GraffitiLocalObject<typeof USER_SETTINGS_SCHEMA>;
+
+  const manager = new PodDelegation(new Ajv());
+  expect(
+    manager.whichPodDelegated(settings, {
+      value: {
+        something: "cool",
+      },
+    }),
+  ).toBe(pod1);
+  expect(
+    manager.whichPodDelegated(settings, {
+      value: {
+        anything: "else",
+      },
+    }),
+  ).toBe(pod2);
+  expect(
+    manager.whichPodDelegated(settings, {
+      noValue: {},
+    }),
+  ).toBe(null);
+});
---
+++ src/pod-delegation.ts
@@ -0,0 +1,48 @@
+import type Ajv from "ajv";
+import type { GraffitiLocalObject } from "./types";
+import { USER_SETTINGS_SCHEMA } from "./schemas";
+
+export default class PodDelegation {
+  constructor(private readonly ajv: Ajv) {}
+
+  delegationFromSettings(
+    settings: GraffitiLocalObject<typeof USER_SETTINGS_SCHEMA>,
+  ) {
+    return settings.value.podDelegation as Array<{
+      pod: string;
+      schema: {};
+    }>;
+  }
+
+  isPodDelegated(
+    settings: GraffitiLocalObject<typeof USER_SETTINGS_SCHEMA>,
+    pod: string,
+    object: {},
+  ) {
+    const delegation = this.delegationFromSettings(settings);
+    const schemas = delegation
+      .filter((delegatedPod) => delegatedPod.pod === pod)
+      .map((delegatedPod) => delegatedPod.schema);
+    for (const schema of schemas) {
+      const validateObject = this.ajv.compile(schema);
+      if (validateObject(object)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  whichPodDelegated(
+    settings: GraffitiLocalObject<typeof USER_SETTINGS_SCHEMA>,
+    object: {},
+  ) {
+    const delegation = this.delegationFromSettings(settings);
+    for (const { pod, schema } of delegation) {
+      const validateObject = this.ajv.compile(schema);
+      if (validateObject(object)) {
+        return pod;
+      }
+    }
+    return null;
+  }
+}

c321e221aebee95043cd8df260ce8e20e676964a
theia <theia@mit.edu>
2025-01-23 23:30:51 -0500

    decode response tests

--- client/src/server-interface/decode-response.spec.ts
+++ client/src/server-interface/decode-response.spec.ts
@@ -5,152 +5,228 @@ import {
   parseGraffitiObjectResponse,
   parseJSONLinesResponse,
 } from "./decode-response";
-// import { randomLocation, randomString, randomValue } from "./test-utils";
+import { randomBase64 } from "@graffiti-garden/implementation-pouchdb";
+import type { GraffitiLocation } from "@graffiti-garden/api";
 
-// it("parse an error string", async () => {
-//   const message = "error message";
-//   const response = new Response(message, { status: 400 });
-//   await expect(catchResponseErrors(response)).rejects.toMatchObject({
-//     message: "error message",
-//   });
-// });
+function randomLocation(): GraffitiLocation {
+  return {
+    name: randomBase64(),
+    actor: randomBase64(),
+    source: randomBase64(),
+  };
+}
 
-// it("parse error without message", async () => {
-//   const response = new Response("", { status: 400 });
-//   const error = await parseErrorResponse(response);
-//   expect(error.message).toContain("400");
-// });
+it("parse an error string", async () => {
+  const message = "error message";
+  const response = new Response(message, { status: 400 });
+  await expect(catchResponseErrors(response)).rejects.toMatchObject({
+    message: "error message",
+  });
+});
 
-// it("parse JSON error", async () => {
-//   const message = "json error message";
-//   const response = new Response(
-//     JSON.stringify({
-//       message,
-//       something: "else",
-//     }),
-//     { status: 400 },
-//   );
-//   const error = await parseErrorResponse(response);
-//   expect(error.message).toBe(message);
-// });
+it("parse JSON error", async () => {
+  const message = "json error message";
+  const response = new Response(
+    JSON.stringify({
+      message,
+      something: "else",
+    }),
+    { status: 404 },
+  );
+  await expect(catchResponseErrors(response)).rejects.toMatchObject({
+    message,
+  });
+});
 
-// it("parse null header", async () => {
-//   const result = parseEncodedStringArrayHeader(null, "default");
-//   expect(result).toBe("default");
-// });
+it("parse null header", async () => {
+  const result = parseEncodedStringArrayHeader(null, "default");
+  expect(result).toBe("default");
+});
 
-// it("parse empty header", async () => {
-//   const result = parseEncodedStringArrayHeader("", "default");
-//   expect(result).toEqual([]);
-// });
+it("parse empty header", async () => {
+  const result = parseEncodedStringArrayHeader("", "default");
+  expect(result).toEqual([]);
+});
 
-// it("encoded header", async () => {
-//   const values = ["🪿", "//++  \n👻"];
-//   const encoded = values.map(encodeURIComponent).join(",");
-//   const result = parseEncodedStringArrayHeader(encoded, "default");
-//   expect(result).toEqual(values);
-// });
+it("encoded header", async () => {
+  const values = ["🪿", "//++  \n👻"];
+  const encoded = values.map(encodeURIComponent).join(",");
+  const result = parseEncodedStringArrayHeader(encoded, "default");
+  expect(result).toEqual(values);
+});
 
-// it("parse put response", async () => {
-//   const response = new Response("", {
-//     status: 201,
-//   });
-//   const location = randomLocation(randomString(), randomString());
-//   const parsed = await parseGraffitiObjectResponse(response, location, false);
-//   expect(parsed.tombstone).toBe(true);
-//   expect(parsed.value).toEqual({});
-//   expect(parsed.channels).toEqual([]);
-//   // expect(parsed.lastModified.getTime()).toBeNaN();
-//   expect(parsed).toMatchObject(location);
-// });
+it("parse no last-modified", async () => {
+  const response = new Response("{}", {
+    status: 200,
+    headers: {
+      "Last-Modified-Ms": "123",
+    },
+  });
+  const location = randomLocation();
+  await expect(
+    parseGraffitiObjectResponse(response, location),
+  ).rejects.toThrow();
+});
 
-// it("parse non-get response", async () => {
-//   const value = { value: "hello" };
-//   const lastModified = new Date();
-//   const channels = ["channel1", "channel2"];
-//   const allowed = ["user1", "user2"];
-//   const response = new Response(JSON.stringify(value), {
-//     status: 200,
-//     headers: {
-//       "Last-Modified": lastModified.toISOString(),
-//       Channels: channels.join(","),
-//       Allowed: allowed.join(","),
-//     },
-//   });
-//   const location = randomLocation(randomString(), randomString());
-//   const parsed = await parseGraffitiObjectResponse(response, location, false);
-//   expect(parsed.tombstone).toBe(true);
-//   expect(parsed.value).toEqual(value);
-//   expect(parsed.channels).toEqual(channels);
-//   expect(parsed.allowed).toEqual(allowed);
-//   // expect(parsed.lastModified.getTime()).toBe(lastModified.getTime());
-//   expect(parsed).toMatchObject(location);
-// });
+it("parse no last-modified-ms", async () => {
+  const response = new Response("{}", {
+    status: 200,
+    headers: {
+      "Last-Modified": new Date().toUTCString(),
+    },
+  });
+  const location = randomLocation();
+  await expect(
+    parseGraffitiObjectResponse(response, location),
+  ).rejects.toThrow();
+});
 
-// it("parse get response", async () => {
-//   const value = { value: "hello" };
-//   const lastModified = new Date();
-//   const channels = ["channel1", "channel2"];
-//   const allowed = ["user1", "user2"];
-//   const response = new Response(JSON.stringify(value), {
-//     status: 200,
-//     headers: {
-//       "Last-Modified": lastModified.toISOString(),
-//       Channels: channels.join(","),
-//       Allowed: allowed.join(","),
-//     },
-//   });
-//   const location = randomLocation(randomString(), randomString());
-//   const parsed = await parseGraffitiObjectResponse(response, location, true);
-//   expect(parsed.tombstone).toBe(false);
-//   expect(parsed.value).toEqual(value);
-//   expect(parsed.channels).toEqual(channels);
-//   expect(parsed.allowed).toEqual(allowed);
-//   // expect(parsed.lastModified.getTime()).toBe(lastModified.getTime());
-//   expect(parsed).toMatchObject(location);
-// });
+it("parse put response", async () => {
+  const date = new Date();
+  const response = new Response("{}", {
+    status: 201,
+    headers: {
+      "Last-Modified": date.toUTCString(),
+      "Last-Modified-Ms": date.getUTCMilliseconds().toString(),
+    },
+  });
+  const location = randomLocation();
+  const parsed = await parseGraffitiObjectResponse(response, location, false);
+  expect(parsed.tombstone).toBe(true);
+  expect(parsed.value).toEqual({});
+  expect(parsed.channels).toEqual([]);
+  expect(parsed.allowed).toEqual(undefined);
+  expect(parsed.lastModified).toEqual(date.getTime());
+  expect(parsed).toMatchObject(location);
+});
 
-// it("parse no channels or allowed or value or timestamp", async () => {
-//   const response = new Response("", {
-//     status: 200,
-//   });
-//   const location = randomLocation(randomString(), randomString());
-//   const parsed = await parseGraffitiObjectResponse(response, location, true);
-//   expect(parsed.tombstone).toBe(false);
-//   expect(parsed.value).toEqual({});
-//   expect(parsed.channels).toEqual([]);
-//   expect(parsed.allowed).toEqual(undefined);
-//   // expect(parsed.lastModified.getTime()).toBeNaN();
-//   expect(parsed).toMatchObject(location);
-// });
+it("parse empty allow", async () => {
+  const date = new Date();
+  const response = new Response(
+    JSON.stringify({
+      hello: "world",
+    }),
+    {
+      status: 200,
+      headers: {
+        "Last-Modified": date.toUTCString(),
+        "Last-Modified-Ms": date.getUTCMilliseconds().toString(),
+        Allowed: "",
+      },
+    },
+  );
+  const location = randomLocation();
+  const parsed = await parseGraffitiObjectResponse(response, location, true);
+  expect(parsed.tombstone).toBe(false);
+  expect(parsed.value).toEqual({ hello: "world" });
+  expect(parsed.channels).toEqual([]);
+  expect(parsed.allowed).toEqual([]);
+  expect(parsed.lastModified).toEqual(date.getTime());
+  expect(parsed).toMatchObject(location);
+});
 
-// it("parse response with extra fields in location", async () => {
-//   const value = randomValue();
-//   const response = new Response(JSON.stringify(value), {
-//     status: 200,
-//   });
-//   const location = {
-//     ...randomLocation(randomString(), randomString()),
-//     value: { not: "what you expect" },
-//     some: "extra fields",
-//   };
+it("parse non-get response", async () => {
+  const value = { value: "hello" };
+  const lastModified = new Date();
+  const channels = ["channel1", "channel2"];
+  const allowed = ["user1", "user2"];
+  const response = new Response(JSON.stringify(value), {
+    status: 200,
+    headers: {
+      "Last-Modified": lastModified.toUTCString(),
+      "Last-Modified-Ms": lastModified.getUTCMilliseconds().toString(),
+      Channels: channels.join(","),
+      Allowed: allowed.join(","),
+    },
+  });
+  const location = randomLocation();
+  const parsed = await parseGraffitiObjectResponse(response, location, false);
+  expect(parsed.tombstone).toBe(true);
+  expect(parsed.value).toEqual(value);
+  expect(parsed.channels).toEqual(channels);
+  expect(parsed.allowed).toEqual(allowed);
+  expect(parsed.lastModified).toBe(lastModified.getTime());
+  expect(parsed).toMatchObject(location);
+});
 
-//   const parsed = await parseGraffitiObjectResponse(response, location, true);
-//   expect(parsed.tombstone).toBe(false);
-//   expect(parsed.value).toEqual(value);
-//   expect(parsed).not.toHaveProperty("some");
-// });
+it("parse get response", async () => {
+  const value = { value: "hello" };
+  const lastModified = new Date();
+  const channels = ["channel1", "channel2"];
+  const allowed = ["user1", "user2"];
+  const response = new Response(JSON.stringify(value), {
+    status: 200,
+    headers: {
+      "Last-Modified": lastModified.toUTCString(),
+      "Last-Modified-Ms": lastModified.getUTCMilliseconds().toString(),
+      Channels: channels.join(","),
+      Allowed: allowed.join(","),
+    },
+  });
+  const location = randomLocation();
+  const parsed = await parseGraffitiObjectResponse(response, location, true);
+  expect(parsed.tombstone).toBe(false);
+  expect(parsed.value).toEqual(value);
+  expect(parsed.channels).toEqual(channels);
+  expect(parsed.allowed).toEqual(allowed);
+  expect(parsed.lastModified).toBe(lastModified.getTime());
+  expect(parsed).toMatchObject(location);
+});
 
-// it("parse response with error", async () => {
-//   const response = new Response("error message", {
-//     status: 400,
-//   });
-//   // const location = randomLocation(randomString(), randomString());
-//   const parsing = parseGraffitiObjectResponse(response, location, true);
-//   await expect(parsing).rejects.toMatchObject({
-//     message: "error message",
-//   });
-// });
+it("bad value", async () => {
+  const response = new Response("{", {
+    status: 200,
+    headers: {
+      "Last-Modified": new Date().toUTCString(),
+      "Last-Modified-Ms": "123",
+    },
+  });
+  const location = randomLocation();
+  await expect(
+    parseGraffitiObjectResponse(response, location),
+  ).rejects.toThrow();
+});
+
+it("parse response with extra fields in location", async () => {
+  const value = {
+    [randomBase64()]: randomBase64(),
+  };
+  const lastModified = new Date();
+  const response = new Response(JSON.stringify(value), {
+    status: 200,
+    headers: {
+      "Last-Modified": lastModified.toUTCString(),
+      "Last-Modified-Ms": lastModified.getUTCMilliseconds().toString(),
+    },
+  });
+  const location = {
+    ...randomLocation(),
+    value: { not: "what you expect" },
+    some: "extra fields",
+  };
+
+  const parsed = await parseGraffitiObjectResponse(response, location, true);
+  expect(parsed.tombstone).toBe(false);
+  expect(parsed.value).toEqual(value);
+  expect(parsed).not.toHaveProperty("some");
+});
+
+it("parse response with error", async () => {
+  const lastModified = new Date();
+  const response = new Response("my error", {
+    status: 400,
+    headers: {
+      "Last-Modified": lastModified.toUTCString(),
+      "Last-Modified-Ms": lastModified.getUTCMilliseconds().toString(),
+    },
+  });
+  const location = randomLocation();
+  await expect(
+    parseGraffitiObjectResponse(response, location),
+  ).rejects.toMatchObject({
+    message: "my error",
+  });
+});
 
 it("parse basic JSON lines", async () => {
   const values = [{ value: "hello" }, { value: "world" }];
